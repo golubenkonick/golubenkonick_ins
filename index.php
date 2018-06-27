a7cb05b5e60481615f61e9968a5d15b0bfd21438
golubenkonick <golubenkonick@gmail.com>
2018-06-27 13:42:46 +0300

    message

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+class Test
+{
+	public function actionSay($name, $message = "Hi ")
+    {
+    	$answer = $message . $name;
+        return $answer;
+    }
+
+	public static function stat()
+    {
+    	$a = 10;
+    	$b = 2;
+    	echo "I am static method";
+        return $a+$b;
+    }
+
+}
+
+
+
+// $t1 = new Test();
+// $m = $t1->actionSay("Nick", "Hello ");
+// echo $m;
+
+echo Test::stat();  // echo 12;
+
+
